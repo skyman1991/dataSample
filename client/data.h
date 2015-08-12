f7c5ea79ae8c8a2e2c2fb418bc58bb04fa62f4d3
Magician <chenhao_ustb@163.com>
2015-08-12 10:43:57 +0800

    classified

--- client/data.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DATA_H
-#define _DATA_H
-
-extern int file_flag; //whitch file to write
-extern void data_write(int _fd);
-extern void data_read(int _fd);
-extern int socket_init();
-#endif
